3539964eafa0d554bb300b496fba54d83b2c12b8
TomaszM-dev <tomekmalocha@gmail.com>
2023-07-17 01:47:05 +0200

    home page

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,15 +1,39 @@
 import formatPrice from "@/util/PriceFormat";
 import React from "react";
 import Image from "next/image";
-import { AddCartType } from "@/types/AddCartType";
+import { ProductType } from "@/types/ProductType";
+import Link from "next/link";
 
-const Product = ({ name, image, price }: AddCartType) => {
+const Product = ({
+  name,
+  image,
+  unit_amount,
+  description,
+  metadata,
+  id,
+}: ProductType) => {
+  const { features } = metadata;
   return (
-    <div>
-      <Image src={image} alt="es" width={400} height={400}></Image>
-      <h1>{name}</h1>
-      {price !== null ? formatPrice(price) : "N/A"}
-    </div>
+    <Link
+      href={{
+        pathname: `/product/${id}`,
+        query: { name, image, unit_amount, description, features, id },
+      }}
+    >
+      <div className="flex items-center flex-col gap-5 mt-20">
+        <Image
+          className="rounded-lg"
+          src={image}
+          alt="es"
+          width={400}
+          height={400}
+        ></Image>
+        <h1 className="text-[2rem] font-[600] uppercase text-main ">{name}</h1>
+        <p className="text-[1.4rem]">
+          {unit_amount !== null ? formatPrice(unit_amount) : "N/A"}
+        </p>
+      </div>
+    </Link>
   );
 };
 
